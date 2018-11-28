ecd238cb965162a7624802362ca4e179b177a004
fauzan_bintang <obin.aji@gmail.com>
2018-11-28 11:57:02 +0700

    done

--- index.js
+++ index.js
@@ -1,1 +1,31 @@
-//your code here
+var arr = []
+function createObj(name, phase, gender) {
+    let obj = {
+        name: name,
+        phase: phase,
+        gender: gender
+    }
+    arr.push(obj)
+}
+
+createObj('akbar', 1, 'male');
+createObj('budi', 2, 'male');
+createObj('icha', 1, 'female');
+
+console.log(arr);
+
+function getData(name) {
+    let result = []
+    arr.forEach(e => {
+        if (e.name === name) {
+            result = e
+        }
+    });
+    if (result.length) {
+        return result
+    } else {
+        return `err getData`
+    }
+}
+
+console.log(getData('icha'));
