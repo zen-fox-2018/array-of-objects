5fe2422939e35aa6f769141c566765fa273e25da
patriagani <patria.gani@gmail.com>
2018-11-28 11:26:24 +0700

    array of objects done

--- index.js
+++ index.js
@@ -1,1 +1,31 @@
-//your code here
+var arr = []
+
+function createObj(name, phase, gender) {
+  let obj = {}
+  obj["name"] = name
+  obj["phase"] = phase
+  obj["gender"] = gender
+
+  arr.push(obj)
+}
+
+createObj("Akbar", 1, "male")
+createObj("Icha", 1, "female")
+createObj("Adhit", 2, "male")
+createObj("Tama", 2, "male")
+createObj("Rifky", 3, "male")
+
+
+
+console.log(arr);
+
+function getData(name) {
+  for (var i = 0; i < arr.length; i++) {
+    if (arr[i].name === name) {
+      return arr[i]
+    }
+  }
+  return "Data tidak ditemukan"
+}
+
+console.log(getData("Icha"));
