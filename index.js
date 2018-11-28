d5ce05df9c328841d052680b9473705e9d032ffc
Luthfi Arfiansyah <luthfi.arfi@gmail.com>
2018-11-28 11:34:34 +0700

    initial commit

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
-//your code here
+var arr = [];
+function CreateObj(name,phase,gender) {
+  var obj = {
+    name : name,
+    phase : phase,
+    gender : gender
+  }
+  arr.push(obj);
+}
+
+CreateObj('luthfi',1,'male');
+CreateObj('Celyn',1,'female');
+CreateObj('Anton',1,'male');
+CreateObj('Ulfa',1,'female');
+
+console.log(arr);
+
+function getData(name) {
+  for (var i = 0; i < arr.length; i++) {
+    if (arr[i].name === name){
+      return  arr[i];
+    }
+  }
+  return -1;
+}
+
+console.log(getData('luthfi'));
