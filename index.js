fa99d9ec1aed25bc0e0ca4edce895e70b3e67dbb
Zhang <zhang@android-42520aa2424d51bc.localdomain>
2018-11-28 14:07:15 +0700

    done

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
-//your code here
+var arr = []
+
+function createObj(name , phase , gender) {
+  let obj = new Object
+  obj.name = name
+  obj.phase = phase
+  obj.gender = gender
+  arr.push(obj)
+}
+createObj('Akbar' , 1 , 'male');
+createObj('Icha' , 1 , 'female')
+
+console.log(arr)
+
+function getData(name) {
+  return arr.filter(x => x.name === name)
+}
+
+console.log(getData('Icha'))
+console.log(getData('Akbar'))
