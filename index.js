01a53f04473c52fede7f1e0b3a4cb0ae6a4f7df9
Mahdihrs <mahdiharis@outlook.com>
2018-11-28 19:55:36 +0700

    membuat array of objects

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
 //your code here
+var arr = []
+function createObj(name, phase, gender) {
+  let result = {
+    name: name,
+    phase: phase,
+    gender: gender
+  }  
+  arr.push(result)
+}
+createObj('Akbar', 1, 'male')
+createObj('Icha', 1, 'female')
+createObj('Budi', 2, 'male')
+createObj('Amin', 3, 'male')
+
+function getData(name) {
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i].name === name) {
+      return arr[i]  
+    }
+  }
+}
+
+console.log(getData('Akbar'))
+console.log(getData('Budi'))
