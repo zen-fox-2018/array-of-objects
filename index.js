d0ceade91e1c6a35392d2203af6f6c8fbf04cdc5
rahmanhasri <rahman.hasri@gmail.com>
2018-11-28 14:58:52 +0700

    maman-array-of-objects

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
 //your code here
+var arr = []
+function createObj(name, phase, gender) {
+  let obj = {
+    name : name,
+    phase : phase,
+    gender : gender,
+  }
+  arr.push(obj)
+}
+
+createObj('Akbar', 1, 'male')
+createObj('Icha', 1, 'female')
+console.log(arr)
+
+function getData(name) {
+  for (let i = 0 ; i <arr.length ; i++) {
+    if (arr[i].name === name){
+      return arr[i]
+    }
+  }
+  return 'Data tidak ditemukan'
+}
+
+console.log(getData('Icha'))
