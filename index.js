d599a9668e65681d4bdcfaebe3f7a359ca024b33
ahmad <ahmad.syukron6991@gmail.com>
2018-11-28 15:49:58 +0700

    object array done

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
 //your code here
+
+var arr = []
+
+function createObj(name,phase,gender){
+    var obj = {}
+    obj.name = name
+    obj.phase = phase
+    obj.gender = gender
+    arr.push(obj)
+}
+createObj('akbar',1, 'male')
+createObj('icha', 1, 'female')
+createObj('adhit',2,'male')
+console.log(arr)
+
+
+var icha = arr[1]
+
+function getData (name){
+    return name
+}
+console.log(getData(icha))
