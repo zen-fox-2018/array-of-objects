011f307ea3a80de82331190484a40f8c2ec30d01
Celyn Susanto <celyn.vy@gmail.com>
2018-11-28 13:54:33 +0700

    done

--- index.js
+++ index.js
@@ -1,1 +1,35 @@
 //your code here
+var arr = [];
+
+function createObj (name, phase, gender) {
+    arr.push({'name' : name, 'phase' : phase, 'gender' : gender})
+    return arr
+}
+createObj ('Akbar', 1, 'male');
+createObj ('Icha', 1, 'female');
+
+console.log(arr)
+/*
+[
+    {
+    name: 'Akbar',
+    phase: 1,
+    gender: 'male'
+    },
+     {
+    name: 'Icha',
+    phase: 1,
+    gender: 'female'
+    },
+
+]
+*/
+function getData (name) {
+    for (let i = 0; i <= arr.length-1; i++) {
+        if (arr[i].name == name) {
+            return arr[i]
+        }
+    }
+}
+console.log(getData('Icha'))
+//{name: 'Icha', phase: 1, gender: 'female'}
