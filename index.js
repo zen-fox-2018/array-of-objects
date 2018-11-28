f1af8a1e213de95f30de132ca4c74e92ba8820e9
rachmadza <razato.zai@gmail.com>
2018-11-28 16:19:23 +0700

    push anchor array of objects

--- index.js
+++ index.js
@@ -1,1 +1,37 @@
 //your code here
+var arr = []
+
+function createObj(name, phase, gender) {
+    let obj = {
+        name : name,
+        phase : phase,
+        gender : gender
+    }
+
+    arr.push(obj)
+}
+
+createObj('Akbar',1,'male')
+createObj('Icha',1,'female')
+createObj('Adhit',2,'male')
+createObj('Tama',2,'male')
+createObj('Rifky',1,'male')
+
+// console.log(arr);
+
+function getData(name) {
+    
+    for (let i = 0; i < arr.length; i++) {
+        // console.log(arr[i]);
+        
+        if (name == arr[i]['name']) {
+            return arr[i]
+        }
+        
+    }
+
+}
+
+console.log(getData('Icha'));
+//{name: 'Icha, phase: 1, gender: 'female}
+
