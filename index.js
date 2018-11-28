c7be8ed4ac622c702f23a4694d338e21dc549af8
Theresia Coanata <theresiacoa@DESKTOP-K5BVEFS.localdomain>
2018-11-28 12:38:31 +0700

    submit task

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
-//your code here
+var arr = [];
+function createObj (name, phase, gender) {
+  var obj = {
+    name: name,
+    phase: phase,
+    gender: gender
+  }
+  arr.push(obj);
+}
+
+createObj('Akbar', 1, 'male');
+createObj('Icha', 1, 'female');
+createObj('Haha', 2, 'male');
+createObj('Rudi', 3, 'male');
+createObj('Victor', 1, 'male');
+
+
+function getData (name) {
+  for (var i = 0; i < arr.length; i++) {      //loop arr (arr is outside of function so it can be used)
+    if (arr[i].name === name) {
+      return arr[i];
+    }
+  }
+}
+console.log(getData('Victor'));
