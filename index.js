7d261846647c386cbbefcb51a2ba5f1ee4374ea5
ACER <fauziaazia171097@gmail.com>
2018-11-28 03:06:06 -0500

    Finish

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
 //your code here
+var arr = []
+function createObj(name,phase,gender) {
+ let obj = {
+     name: name,
+     phase:phase,
+     gender:gender
+ }
+ arr.push(obj)
+}
+function getData(name) {
+    let data = arr.forEach(element =>  {
+        if(element.name === name){
+           console.log(element)
+        }
+    });
+
+}
+createObj("Akbar",1,"male")
+createObj("Icha",1,"female")
+
+// console.log(arr)
+getData("dadang")
