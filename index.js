3852dc27ac9b3f723aa12b92861f60e2ce8dde56
Aditya Respati <AdityaRespati>
2018-11-28 19:21:43 +0700

    added solution

--- index.js
+++ index.js
@@ -1,1 +1,33 @@
 //your code here
+
+var arr = []
+
+function createObj(name, phase, gender) {
+    let objPerson = {
+        name: name,
+        phase: phase,
+        gender: gender
+    }
+
+    arr.push(objPerson)
+}
+
+createObj('Akbar', 1, 'male')
+createObj('Icha', 1, 'female')
+createObj('Adhit', 2, 'male')
+createObj('Tama', 2, 'male')
+createObj('Rifky', 3, 'male')
+
+console.log(arr)
+
+function getData(name) {
+    for (let i = 0; i < arr.length; i++){
+        if(arr[i].name == name){
+            return arr[i]
+        }
+    }
+}
+
+console.log(getData('Icha'))
+
+
