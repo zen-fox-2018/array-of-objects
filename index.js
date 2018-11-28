b6f38e844613a307ecba2f0483dbfe688d662e31
ShadowM8 <anton2014lenovo@gmail.com>
2018-11-28 12:31:09 +0700

    array-of-object done -anton-

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
-//your code here
+var arr = []
+
+function createObj (name,phase, gender) {
+    let result = {}
+    result['name'] = name
+    result['phase'] = phase
+    result['gender'] = gender
+
+    arr.push(result)
+    
+}
+
+function getData(name) {
+    let result = {}
+    for (let i = 0; i < arr.length; i++){
+        if(name === arr[i].name){
+            result = arr[i]
+        }
+    }
+    return result
+}
+
+createObj('Akbar', 1, 'male')
+createObj('Icha', 1, 'female')
+
+console.log(arr)
+console.log(getData('Icha'))
